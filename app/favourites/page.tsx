abf6dbaa582599f608a14c03b58fb021b1e32bbb
AsokTamang <ashoktmg2058@gmail.com>
2025-07-04 00:52:41 -0400

    ui as well as server side for adding the comment on the fav deal done

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -2,9 +2,27 @@
 import React from "react";
 import { valueStore } from "@/store/dataStorage";
 import toast from "react-hot-toast";
+import { GoPlus } from "react-icons/go";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+
+
+interface commentType{
+    [link:string]:string     //here we are defining the state type which is a key-value pair having link as a key and comment as a pair
+}
+interface valueType{
+    link:string;
+    comment:string;
+}
+interface shownType {
+    [link:string]:boolean;
+}
 
 export default function Favourites() {
   const { fetchFavourites, favourites } = valueStore();
+  const [shown, setShown] = React.useState<shownType>({});
+  const [comments, setComments] = React.useState<commentType>({});
 
   React.useEffect(() => {
     const fetchDatas = async () => {
@@ -26,13 +44,31 @@ export default function Favourites() {
     };
     fetchDatas();
   }, []);
+  const handleClick = async (value:valueType) => {
+    try {
+      setShown((prev)=>({...prev,[value.link]:false}));   //here when the save comment button of that specific deal is clicked then we make the shown state ofthat link false
+      const res = await axios.post("/api/addcomment", { value });
+      const { success, data, message } = res.data;
+      if (success) {
+        toast.success(message);
+      } else {
+        toast.error(message);
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    }
+  };
 
   const elements = favourites.map((item, i) => (
     <div
       key={i}
       className="bg-white rounded-lg shadow-md p-4 mb-4 hover:shadow-lg transition-shadow max-w-4xl mx-auto"
     >
-      <h2 className="text-xl font-semibold text-gray-900 truncate mb-1">{item.title}</h2>
+      <h2 className="text-xl font-semibold text-gray-900 truncate mb-1">
+        {item.title}
+      </h2>
       <a
         href={item.link}
         target="_blank"
@@ -41,6 +77,25 @@ export default function Favourites() {
       >
         {item.link}
       </a>
+
+      {!shown[item.link] ? (
+        <>
+          <span>Add a comment for your favorite deal</span>
+          <button className="hover:text-2xl" onClick={() => setShown((prev)=>({...prev,[item.link]:true}))}>
+            <GoPlus />
+          </button>
+        </>
+      ) : (
+        <>
+          <Textarea
+            
+            onChange={(e)=>{setComments((prevState)=>({...prevState,[item.link]:e.target.value}))}}
+          />
+          <Button onClick={() => handleClick({link:item.link, comment:comments[item.link]})}>
+            Save comment
+          </Button>
+        </>
+      )}
     </div>
   ));
 
